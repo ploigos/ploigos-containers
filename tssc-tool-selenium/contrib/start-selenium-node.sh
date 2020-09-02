b1f7b188e94f555653b351b9edad308b22e327b1
Hunter Gerlach <hgerlach@redhat.com>
2020-09-02 17:01:57 -0400

    Remove uncalled commands

--- tssc-tool-selenium/contrib/start-selenium-node.sh
+++ tssc-tool-selenium/contrib/start-selenium-node.sh
@@ -14,19 +14,6 @@ JAVA_OPTS="${JAVA_OPTS} -Dwebdriver.chrome.whitelistedIps"
 echo "Starting Selenium Hub with configuration:"
 cat ${CONF}
 
-
-#/opt/bin/generate_chrome_config > /opt/selenium_node/config.json
-
-## Start the pulseaudio server
-#pulseaudio -D --exit-idle-time=-1
-
-## Load the virtual sink and set it as default
-#pacmd load-module module-virtual-sink sink_name=v1
-#pacmd set-default-sink v1
-
-## set the monitor of v1 sink to be the default source
-#pacmd set-default-source v1.monitor
-
 rm -f /tmp/.X*lock
 echo "JAVA_OPTS: ${JAVA_OPTS}"
 echo "HUB_HOST: ${HUB_HOST}"
