b7e3f31d5050c9004d8ce086abf157dc34fa91aa
Ian Tewksbury <itewk@redhat.com>
2021-07-28 15:41:53 +0000

    default the podman/buildah/skopeo storage driver to VFS because that is what works in rootless mode. NOTE: brand new kernels can support rootless overlay but it isn't wide spread enough yet to be useful, but when it is can be overriden with env var and the default can be changed when it makes sense

--- ploigos-tool-jkube/ploigos-tool-jkube-entrypoint.sh
+++ ploigos-tool-jkube/ploigos-tool-jkube-entrypoint.sh
@@ -7,7 +7,6 @@ echo $(whoami):10000:65536 >> /etc/subgid
 
 # create a podman socket for JKube to use
 # SEE https://github.com/fabric8io/docker-maven-plugin/issues/1330#issuecomment-872905283
-export DOCKER_HOST="unix:/home/ploigos/podman.sock"
 podman system service --time=0 ${DOCKER_HOST} 1> /home/ploigos/podman.stdout 2> /home/ploigos/podman.stderr &
 
 # call the base entrypoint
