7caa1c2e891512209c95da53ffddbec0b8b2dad7
Deven Phillips <deven.phillips@redhat.com>
2021-08-27 09:40:48 -0400

    Set the `-dir` parameter

--- ploigos-tool-zap/updateAddons.sh
+++ ploigos-tool-zap/updateAddons.sh
@@ -6,5 +6,5 @@ for I in $(echo $ZAPROXY_ADDONS); do
   ADDON_OPTIONS=${ADDON_OPTIONS}" -addoninstall ${I}"
 done
 
-./zap.sh -addonupdate -cmd
-./zap.sh ${ADDON_OPTIONS} -cmd
+./zap.sh -dir /home/.ZAP -addonupdate -cmd
+./zap.sh -dir /home/.ZAP ${ADDON_OPTIONS} -cmd
